69abf5f5c5ea7aa4b2dfae4da65e6ca87cca0ecc
laurensruiz <laurenruiz97@yahoo.com>
2022-05-17 11:52:39 -0400

    setup router

--- api/cars/cars-router.js
+++ api/cars/cars-router.js
@@ -1,1 +1,18 @@
 // DO YOUR MAGIC
+const express = require('express')
+
+const router = express.Router()
+
+router.get('/', async (req, res, next)=> {
+    res.json('get all')
+})
+
+router.get('/:id', async (req, res, next)=> {
+    res.json('get by id')
+})
+
+router.post('/', async (req, res, next)=> {
+    res.json('post')
+})
+
+module.exports = router
--- api/server.js
+++ api/server.js
@@ -1,14 +1,16 @@
 const express = require("express")
-
+const carsRouter = require('./cars/cars-router')
 const server = express()
 
 // DO YOUR MAGIC
 //parse
 server.use(express.json())
 
+//router
+server.use('/api/cars', carsRouter)
 //catchall
 server.use('*', (req, res, next)=> {
-    next({status:404, message:'not found'})
+    next({status:404, message:'not found!'})
 })
 
 //error handle
