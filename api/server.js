aa09c428cd89cf83b0afaca52eed5151617d54f9
laurensruiz <laurenruiz97@yahoo.com>
2022-05-17 11:17:49 -0400

    did migrations

--- api/server.js
+++ api/server.js
@@ -2,7 +2,6 @@ const express = require("express")
 
 const server = express()
 
-server.use = require(express())
 // DO YOUR MAGIC
 
 module.exports = server
--- data/migrations/01-make_cars_table.js
+++ data/migrations/01-make_cars_table.js
@@ -1,7 +1,25 @@
 exports.up = function (knex) {
   // DO YOUR MAGIC
+  //unsigned means no negative
+  return knex.schema.createTable('cars', tbl => {
+    tbl.increments('id')
+
+    tbl.string('vin', 17).notNullable().unique()
+
+    tbl.string('make', 128).notNullable()
+
+    tbl.string('model', 128).notNullable()
+
+    tbl.numeric('mileage').unsigned().notNullable() 
+
+    tbl.string('title', 128)
+
+    tbl.string('transmission', 128)
+  })
 };
 
+//dowm is reverse of up
 exports.down = function (knex) {
   // DO YOUR MAGIC
+  return knex.schema.dropTableIfExists('cars')
 };
