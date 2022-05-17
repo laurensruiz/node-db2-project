b646bab8bb7d76a45435ba93bca86a1d1c948c0c
laurensruiz <laurenruiz97@yahoo.com>
2022-05-17 11:36:43 -0400

    made server usable

--- api/server.js
+++ api/server.js
@@ -3,5 +3,21 @@ const express = require("express")
 const server = express()
 
 // DO YOUR MAGIC
+//parse
+server.use(express.json())
+
+//catchall
+server.use('*', (req, res, next)=> {
+    next({status:404, message:'not found'})
+})
+
+//error handle
+
+server.use((err, req, res, next) => { //eslint-disable-line
+    res.status(err.status || 500).json({
+        message:err.message
+    })
+})
+
 
 module.exports = server
